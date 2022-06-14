ce40059f3e34acc974e971b4b7194ca4d40bdd28
Abdul Muqeet Arshad <abdulmuqeet.arshad@devsinc.com>
2022-06-14 14:19:15 +0500

    added buyCake action export in index.js

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { buyCake } from '../redux/cake/cakeActions';
+import { buyCake } from '../redux';
 
 const CakeContainer = ({ noOfCakes, buyCake }) => {
 	return (
--- src/components/HookCakeContaner.jsx
+++ src/components/HookCakeContaner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { buyCake } from '../redux/cake/cakeActions';
+import { buyCake } from '../redux';
 
 const HookCakeContaner = () => {
 	const noOfCakes = useSelector(state => state.noOfCakes);
